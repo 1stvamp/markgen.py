b9e961fa4e0c97848606cf15bce95730e7e2c69e
Wes Mason <wes@1stvamp.org>
2014-01-04 13:35:41 +0000

    Bumpt 0.9.4 and fix package_data being included, not needed and causes error on install

--- setup.py
+++ setup.py
@@ -8,12 +8,10 @@ setup(
     description='Python library for the programmatic generation of markdown text',
     long_description=open('README.rst').read(),
     provides=['markgen'],
-    version='0.9.3',
+    version='0.9.4',
     author='Wes Mason',
     author_email='wes@1stvamp.org',
     url='https://github.com/1stvamp/markgen.py',
     packages=find_packages(exclude=['markgen_tests']),
-    package_data={'': 'markgen_test_doc.md'},
-    include_package_data=True,
     license='BSD'
 )
